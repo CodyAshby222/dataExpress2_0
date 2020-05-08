96ddb10b3341cdc31f9fbd435d233509cac7119c
CodyAshby222 <60458709+CodyAshby222@users.noreply.github.com>
2020-05-08 13:50:55 -0600

    dun did le first commit

---
+++ routes/routes.js
@@ -0,0 +1,27 @@
+let mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://AnibalTR:xFeg7GR8oCkmv4dS@cluster0-gipdv.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+
+let mdb = mongoose.connection;
+mdb.on("error", console.error.bind(console, "connection error:"));
+mdb.once("open", (callback) => {});
+
+let personSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  age: String,
+  firstQuestion: String,
+  secondQuestion: String,
+  thirdQuestion: String,
+});
+
+let Person = mongoose.model("User_Information", personSchema);
+
+// exports.index
